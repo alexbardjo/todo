ab90b8af15af7a5df084e31d5377f711edf934bf
Alexbardjo <alexbardjo@gmail.com>
2020-03-09 13:10:17 +0200

    Structure React project vol.2

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (<input type="text"
+                   className="search-input form-control"
+                   placeholder='type to search'/>
+    );
+};
+
+export default SearchPanel;
